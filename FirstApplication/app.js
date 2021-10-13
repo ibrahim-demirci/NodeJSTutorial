7ac7e85ff91433f554fa3631eab8990e3bec86b2
ibrahim-demirci <ibrahim.demirci@marun.edu.tr>
2021-10-13 15:17:29 +0300

    electronic controller added and code more readable

--- FirstApplication/app.js
+++ FirstApplication/app.js
@@ -3,23 +3,22 @@ const { profile } = require('console');
 
 var express = require('express');
 const path = require('path');
+
+var ctrlElectronic = require('./ElectronicController');
+
+
+
+
+
+
 var app = express();
 
 app.use('/public', express.static(path.join(__dirname, 'public')));
 
-var indexController = function (req, res) {
 
-    res.sendFile(path.join(__dirname, 'index.html'));
 
-}
-var profileController = function (req, res) {
-
-    res.sendFile(path.join(__dirname, 'profile.html'));
-
-}
-
-app.get('/', indexController);
-app.get('/profile', profileController);
+app.get('/electronic', ctrlElectronic.index);
+app.get('/electronic/computer', ctrlElectronic.computer);
 
 app.listen(8000);
 
