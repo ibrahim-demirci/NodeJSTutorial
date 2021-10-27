05623d18c06483a4549e7b79ffa6e126fe513f3a
ibrahim-demirci <ibrahim.demirci@marun.edu.tr>
2021-10-27 15:41:56 +0300

    page get methods added with router objedct

--- FirstApplication/ElectronicController.js
+++ FirstApplication/ElectronicController.js
@@ -1,4 +1,8 @@
 var path = require('path')
+var express = require('express')
+var router = express.Router();
+
+
 
 module.exports.index = function (req, res) {
 
@@ -8,4 +12,15 @@ module.exports.computer = function (req, res) {
 
     res.sendFile(path.join(__dirname, 'profile.html'));
 }
+
+
+router.get('/electronic', module.exports.index)
+router.get('/electronic/computer', module.exports.computer)
+
+module.exports = router;
+
+
+
+
+
 
--- FirstApplication/app.js
+++ FirstApplication/app.js
@@ -4,21 +4,15 @@ const { profile } = require('console');
 var express = require('express');
 const path = require('path');
 
-var ctrlElectronic = require('./ElectronicController');
-
-
-
-
+var routeElectronic = require('./ElectronicController');
 
 
 var app = express();
 
 app.use('/public', express.static(path.join(__dirname, 'public')));
 
+app.use('/', routeElectronic)
 
-
-app.get('/electronic', ctrlElectronic.index);
-app.get('/electronic/computer', ctrlElectronic.computer);
 
 app.listen(8000);
 
